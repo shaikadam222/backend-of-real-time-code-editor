e1c220e8b5c0741055f53d8d9a048706a78d6604
Shaik <shaikadam642@gmail.com>
2024-02-12 08:57:35 +0530

    one more verification

--- backend.js
+++ backend.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json());
-app.get('/',(req,res) => {
+app.get('/code',(req,res) => {
     res.send("got my get request").status(200)
 })
 app.post('/code', async (req, res) => {
